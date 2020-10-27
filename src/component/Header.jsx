8617b1f139e112974878e6e545071c139c583a1e
Guy de Messimy <gdemessimy@gmail.com>
2020-10-27 11:35:12 +0100

    add header component and import from App

---
+++ src/component/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+return (
+    < div class="header">
+        <div class="logo-iss">
+            <a href="https://placeholder.com"></a>
+            <img src="https://via.placeholder.com/1360x50"></img>
+        </div>
+    </div>
+);
+}
+
+export default Header;
+
+
+   
